f84913836197ec3fb19c14f3dfb99dd4a67001b1
Carlos <carlos.pages@nanoporetech.com>
2022-01-14 12:16:34 +0100

    Initial Commit

---
+++ Shared/FooApp.swift
@@ -0,0 +1,17 @@
+//
+//  FooApp.swift
+//  Shared
+//
+//  Created by Carlos Pages on 14/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct FooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
